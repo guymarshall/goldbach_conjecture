1187ca60033c7aa2dfe481dfcc9b43cd90208805
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-10 14:26:38 +0100

    UPDATE: Use much more efficient primes_add_up_to_number function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::process::exit;
 use std::time::Instant;
 
-const COUNT: i32 = 1_000_000;
-
 const fn int_sqrt(number: i32) -> i32 {
     let mut x: i32 = number;
     let mut y: i32 = (x + 1) / 2;
@@ -34,42 +32,27 @@ const fn is_prime(number: i32) -> bool {
     true
 }
 
-fn generate_primes() -> [i32; COUNT as usize] {
-    let mut primes: [i32; COUNT as usize] = [0i32; COUNT as usize];
-    let mut count: i32 = 0;
-    let mut number: i32 = 2;
-
-    while count < COUNT {
-        if is_prime(number) {
-            primes[count as usize] = number;
-            count += 1;
-        }
-        number += 1;
+fn primes_add_up_to_number(number_to_check: i32) -> bool {
+    if is_prime(number_to_check - 2) {
+        return true;
     }
-
-    primes
-}
-
-fn primes_add_up_to_number(primes: &[i32], number_to_check: i32) -> bool {
-    primes.iter().any(|prime: &i32| {
-        primes.contains(&(number_to_check - prime))
-    })
+    (3..=number_to_check)
+        .step_by(2)
+        .filter(|number| is_prime(*number))
+        .any(|number| { is_prime(number_to_check - number) })
 }
 
 fn main() {
     let start_time: Instant = Instant::now();
 
+    const COUNT: i32 = 1_000_000;
     const LIMIT: i32 = COUNT * 1000;
 
-    println!("Generating primes upto {COUNT}...");
-    let primes: [i32; COUNT as usize] = generate_primes();
-
-    println!("Checking sums of primes...");
     (4..LIMIT).step_by(2).for_each(|number_to_check: i32| {
         if number_to_check % 1000 == 0 {
             println!("{:.4}%", (number_to_check as f64 / LIMIT as f64) * 100.0);
         }
-        if !primes_add_up_to_number(&primes, number_to_check) {
+        if !primes_add_up_to_number(number_to_check) {
             println!("No two primes add to make {number_to_check}");
             exit(0);
         }
