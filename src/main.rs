739cb6bfa098554db9265cd32ad672e77f414a06
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-11-25 21:45:14 +0000

    UPDATE: Print results on same line

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,14 @@ fn main() {
         .par_bridge()
         .for_each(|number_to_check: usize| {
             if number_to_check % 10000 == 0 {
-                println!("{:.4}%", (number_to_check as f64 / LIMIT as f64) * 100.0);
+                print!("\r{:.4}%", (number_to_check as f64 / LIMIT as f64) * 100.0);
             }
             if !primes_add_up_to_number(number_to_check) {
-                println!("No two primes add to make {number_to_check}");
+                println!("\nNo two primes add to make {number_to_check}");
                 exit(0);
             }
         });
+    println!();
 
     let end_time: Instant = Instant::now();
 
